64d0e03b6406c943e164f45e6d91d1bd589a4de3
Yawuar <yawuarsernadelgado@hotmail.be>
2018-05-14 15:22:09 +0200

    added laravel project and added authentication

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+
+use App\User; 
+
+class ApiController extends Controller
+{
+    public $successStatus = 200;
+
+    public function login(){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('washrobe')-> accessToken; 
+            return response()->json(['success' => $success], $this-> successStatus); 
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
+    }
+
+    public function register(Request $request) 
+    { 
+        $validator = Validator::make($request->all(), [ 
+            'firstname' => 'required', 
+            'lastname' => 'required', 
+            'email' => 'required|email', 
+            'gender' => 'required',
+            'password' => 'required', 
+            'c_password' => 'required|same:password', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+        $input = $request->all(); 
+        $input['password'] = bcrypt($input['password']); 
+        $user = User::create($input); 
+        $success['token'] =  $user->createToken('washrobe')->accessToken; 
+        $success['firstname'] =  $user->firstname;
+
+        return response()->json(['success'=>$success], $this->successStatus); 
+    }
+
+    public function getUser() 
+    { 
+        $user = Auth::user(); 
+        return response()->json(['success' => $user], $this->successStatus); 
+    } 
+}
---
+++ app/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstname', 'lastname', 'gender', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
