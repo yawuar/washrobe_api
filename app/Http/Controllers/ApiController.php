84bd8d4135bdd0514f3c6e0c3b014296829f5ffb
Yawuar <yawuarsernadelgado@hotmail.be>
2018-05-16 17:54:33 +0200

    added validation in login

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,20 +13,30 @@ class ApiController extends Controller
     public $successStatus = 200;
 
     public function login(){ 
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
-            $user = Auth::user(); 
-            $success['token'] =  $user->createToken('washrobe')-> accessToken; 
-            return response()->json(['success' => $success], $this-> successStatus)->withHeaders([
-                'Access-Control-Allow-Origin', '*',
-                'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'
-            ]); 
-        } 
-        else{ 
-            return response()->json(['error'=>'Unauthorised'], 401)->withHeaders([
-                'Access-Control-Allow-Origin', '*',
-                'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'
-            ]); 
-        } 
+
+        $validator = Validator::make($request->all(), [ 
+            'email' => 'required|email', 
+            'password' => 'required', 
+        ]);
+
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        } else {
+            if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+                $user = Auth::user(); 
+                $success['token'] =  $user->createToken('washrobe')->accessToken; 
+                return response()->json(['success' => $success], $this-> successStatus)->withHeaders([
+                    'Access-Control-Allow-Origin', '*',
+                    'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'
+                ]); 
+            } 
+            else{ 
+                return response()->json(['error'=>'Unauthorised'], 401)->withHeaders([
+                    'Access-Control-Allow-Origin', '*',
+                    'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'
+                ]); 
+            } 
+        }
     }
 
     public function register(Request $request) 
@@ -39,9 +49,11 @@ class ApiController extends Controller
             'password' => 'required', 
             'c_password' => 'required|same:password', 
         ]);
+
         if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
+
         $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
