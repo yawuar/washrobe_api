d7eaf7c9fde0bba80b72eeb07072e21997a12e94
Yawuar <yawuarsernadelgado@hotmail.be>
2018-05-23 21:23:03 +0200

    added few extra cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'localhost:8100')
+        ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Credentials', 'true')
         ->header('Access-Control-Allow-Headers',  'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
