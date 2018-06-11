df8d0ea397eb0376b0ce369add7788c47e854fc3
Yawuar <yawuarsernadelgado@hotmail.be>
2018-06-11 13:31:35 +0200

    Made some changes to the return values

--- app/Http/Controllers/LaundryController.php
+++ app/Http/Controllers/LaundryController.php
@@ -71,7 +71,6 @@ class LaundryController extends Controller
     public function putInLaundry($id) {
         $user = Auth::user();
         $items = User::find($user['id'])->items()->where('item_id', $id)->get();
-
         $message = [];
 
         $index = 0;
@@ -100,21 +99,22 @@ class LaundryController extends Controller
                 for($i = 0; $i < $length; $i++) {
                     $pivotID = $items[$index]->pivot->id;
                     $checkIfIsLaundry = Laundry::where('user_itemID', $pivotID)->get();
+                    var_dump(count($checkIfIsLaundry));
                     if(count($checkIfIsLaundry) > 0) {
                         $index++;
                     } else {
                         Laundry::create(['user_itemID' => $pivotID]);
                         array_push($message, [
-                            message => 'The item is successfully added to the laundry',
-                            alreadyInLaundry => false
+                            'message' => 'The item is successfully added to the laundry',
+                            'alreadyInLaundry' => false
                         ]);
                         break;
                     }
                 }
             } else {
                 array_push($message, [
-                    message => 'This item is already in the laundry',
-                    alreadyInLaundry => true
+                    'message' => 'This item is already in the laundry',
+                    'alreadyInLaundry' => true
                 ]);
             }
         }
